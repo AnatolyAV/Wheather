f2f383e47881d096e88dcbc62c977557b895304a
AnatolyAV <kidrockjr2008@yandex.ru>
2018-03-05 23:05:42 +0300

    Fix при наследовании интерфейсов с стратегиями от других интерфейсов с стратегиями применяются стратегии наследников, поэтому для выполнения родительских нужных стратегий методов надо выставить стратегии именно на них,а не на весь интерфейс

--- app/src/main/java/ru/andreev_av/weather/presentation/views/INotConnectionView.java
+++ app/src/main/java/ru/andreev_av/weather/presentation/views/INotConnectionView.java
@@ -4,8 +4,8 @@ import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
-@StateStrategyType(SkipStrategy.class)
 public interface INotConnectionView extends MvpView {
 
+    @StateStrategyType(SkipStrategy.class)
     void showNotConnection();
 }
